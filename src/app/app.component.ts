cad2bcc29f2a5d69e7b5c5e478f9e6760d9068ef
xuan622 <34533348+xuan622@users.noreply.github.com>
2018-11-19 05:37:17 +0800

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+<div style="text-align:center; align-content:center">
+    <h1>{{title}}</h1>
+    <input #newItem
+      (keyup.enter)="addItem(newItem.value)">
+ 
+    <button (click)="addItem(newItem.value)">Add</button>
+    <button (click)="clearItem()">Reset</button>
+    
+ 
+    <ul><li *ngFor="let item of items">{{item}}</li></ul>
+ </div>
+  `
+})
+export class AppComponent {
+  title = 'To Do List';
+  items = ['Do Homework', 'Surfing Internet', 'Buy dairy products'];
+  addItem(newItem: string) {
+    if (newItem) {
+      this.items.push(newItem);
+    }
+  }
+  clearItem() {
+    this.items = [];
+  }
+  
+}
